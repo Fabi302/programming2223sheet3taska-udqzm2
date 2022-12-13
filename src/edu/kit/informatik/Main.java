45170a3e252d69de43dddede51d1f8602c27f0f8
Fabian Rappert <fabianrappert03@gmail.com>
2022-12-13 22:31:52 +0100

    .

--- src/edu/kit/informatik/Main.java
+++ src/edu/kit/informatik/Main.java
@@ -1,5 +1,45 @@
 package edu.kit.informatik;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        TicTacToe playField = new TicTacToe(new String[9]);
+
+        playField.emptyField();
+        playField.fieldFormation();
+
+        System.out.println(playField.fieldFormation());
+
+        Scanner scanner = new Scanner(System.in);
+        String player = "x";
 
 
+        for(int i = 1; i < 10; i++) {
+            System.out.println(i + ". Zug:" + player);
+            String input = scanner.nextLine();
+            if(!playField.isNotOccupied(Integer.parseInt(input))){
+                i--;
+                continue;
+            }
+            playField.placeMark(Integer.parseInt(input), player);
+            System.out.println(playField.fieldFormation());
+
+            if(playField.playerWon()) {
+                System.out.println("Sieger:" + player);
+                break;
+            }
+            else if(playField.fieldFull()) {
+                System.out.println("Kein Sieger");
+                break;
+            }
+            if(player.equals("x")) {
+                player = "o";
+            }
+            else {
+                player = "x";
+            }
+        }
+
+    }
 }
--- src/edu/kit/informatik/TicTacToe.java
+++ src/edu/kit/informatik/TicTacToe.java
@@ -1,23 +1,24 @@
 package edu.kit.informatik;
 
-import java.util.Scanner;
 /**
  * public class TicTacToe contains every method
  */
 public class TicTacToe {
     /**
-     * playField is the 3x3 Field used in the game
+     * playField is the 3x3 Field used in the game represented through an Array with the length 9
      */
-    String[] playField = new String[9];
+    String[] playField;
+    public TicTacToe (String[] playField) {
+        this.playField = playField;
+    }
 
     /**
      * the method emptyField creates the default playField, filled with "-"
      */
-    public String[] emptyField() {
+    public void emptyField() {
         for(int i = 0; i < playField.length; i++) {
             playField[i] = "-";
         }
-        return playField;
     }
 
     /**
@@ -35,23 +36,23 @@ public class TicTacToe {
      * @return returns "won" if the game is won
      */
     public boolean playerWon() {
-        boolean won = false;
         for(int i = 0; i < 3; i++) {
-            if(playField[0 +i].equals(playField[1 + i].equals(playField[2 + i]))) { // checks vertical
-                won = true;
+            if(playField[i * 3].equals(playField[1 + i * 3]) && playField[1 + i *3].equals(playField[2 + i * 3])
+                && !playField[i *3].equals("-")) { // checks vertical
+                return true;
             }
-            else if(playField[0 +i].equals(playField[3 +i].equals(playField[6 + i]))) { // checks horizontal
-                won = true;
+            else if(playField[i].equals(playField[3 +i]) && playField[3 + i].equals(playField[6 + i])
+                && !playField[i].equals("-")) { // checks horizontal
+                return true;
             }
         }
-        if(playField[0].equals(playField[4].equals(playField[8]))) { // checks diagonal from left to right
-            won = true;
+        if(playField[0].equals(playField[4]) && playField[4].equals(playField[8]) && !playField[0].equals("-")) { // checks diagonal from left to right
+            return true;
         }
-        if(playField[2].equals(playField[4].equals(playField[6]))) { // checks diagonal from right to left
-            won = true;
+        if(playField[2].equals(playField[4]) && playField[4].equals(playField[6]) && !playField[2].equals("-")) { // checks diagonal from right to left
+            return true;
         }
-        return won;
-
+        return false;
     }
 
     /**
@@ -60,8 +61,9 @@ public class TicTacToe {
      */
     public boolean fieldFull() {
         for(int i = 0; i < playField.length; i++) {
-            if(playField[i] != "-") {
+            if(playField[i].equals("-")) {
             }
+            return false;
         }
         return true;
     }
@@ -77,11 +79,78 @@ public class TicTacToe {
     /**
      * method fieldFormation creates the wanted formation with 3 letters in one row
      */
-    public void fieldFormation() {
+    public String fieldFormation() {
         String rowOne = playField[0] + playField[1] + playField [2];
         String rowTwo = playField[3] + playField[4] + playField [5];
         String rowThree = playField [6] + playField[7] + playField[8];
         String fieldFormation = rowOne + "\n" + rowTwo + "\n" + rowThree;
+        return fieldFormation;
+    }
+
+    /**
+     * method checks if there is a possible win in one of the rows
+     * @return if there is a possible win the index of the empty field needed is returned,
+     * if there is no possible Win "-1" is returned
+     */
+    public int possibleWinRow() {
+        for(int i = 0; i < 3; i++) {
+            if(playField[i * 3].equals("-") && playField[1 + i * 3].equals(playField[2 + i * 3])) {
+                return i * 3;
+            }
+            else if(playField[1 + i * 3].equals("-") && playField[i * 3].equals(playField[2 + i * 3])) {
+                return 1 + i * 3;
+            }
+            else if(playField[2 + i * 3].equals("-") && playField[i * 3].equals(playField[1 + i * 3])) {
+                return 2 + i * 3;
+            }
         }
+        return -1;
+    }
+
+    /**
+     * method checks if there is a possible win in one of the verticals
+     * @return if there is a possible win the index of the empty field needed is returned,
+     * if there is no possible win "-1" is returned
+     */
+    public int possibleWinVertical() {
+        for(int i = 0; i < 3; i++) {
+            if(playField[i].equals("_") && playField[3 + i].equals(playField[6 + i])) {
+                return i;
+            }
+            else if(playField[3 + i].equals("_") && playField[i].equals(playField[6 + i])) {
+                return 3 + i;
+            }
+            else if(playField[6 + i].equals("_") && playField[i].equals(playField[3 + i])) {
+                return 6 + i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * method checks if there is a possible win in one of the diagonals
+     * @return if there is a possible win the index of the empty field needed is returned,
+     * if there is no possible win "-1" is returned
+     */
+    public int possibleWinDiagonal() {
+        if(playField[0].equals("-") && playField[4].equals(playField[8])) {
+            return 0;
+        }
+        else if(playField[2].equals("-") && playField[4].equals(playField[6])) {
+            return 2;
+        }
+        else if(playField[4].equals("-") && playField[0].equals(playField[8])) {
+            return 4;
+        }
+        else if(playField[4].equals("-") && playField[2].equals(playField[6])) {
+            return 4;
+        }
+        else if(playField[6].equals("-") && playField[2].equals(playField[4])) {
+            return 6;
+        }
+        else if(playField[8].equals("-") && playField[0].equals(playField[4])) {
+            return 8;
+        }
+        return -1;
     }
 }
